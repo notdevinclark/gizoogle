15b4fb7a783f48ea084491496d73b143a4fa0a8d
Devin Clark <notdevinclark@gmail.com>
2016-03-02 16:25:41 -0600

    updates gizoogle module wit tha url function
    this function returns a url wit given query ta a joint dat is ghon be
    translated tha fuck into pure thug

--- lib/gizoogle.ex
+++ lib/gizoogle.ex
@@ -3,7 +3,7 @@ defmodule Gizoogle do
   A client for gizoogle.net
   """
 
-  @translate_url "http://www.gizoogle.net/textilizer.php"
+  @base_url "http://www.gizoogle.net"
 
   @doc """
   Translates yo' text so you can drop a rhyme like a thug
@@ -15,12 +15,28 @@ defmodule Gizoogle do
   """
   @spec translate(String.t) :: String.t
   def translate(text) do
-    HTTPoison.post(@translate_url, {:form, [translatetext: text]})
+    HTTPoison.post("#{@base_url}/textilizer.php", {:form, [translatetext: text]})
     |> handle_response
   end
 
   defp handle_response({:error, reason}), do: "error: #{reason}"
   defp handle_response({:ok, %HTTPoison.Response{body: body}}) do
     Gizoogle.Parser.parse_textilizer(body)
+  end
+
+  @doc """
+  Takes a query strang n' returns a url dat is ghon be translated ta be pure thug
+
+  When given a url, it translates tha page, otherwise it returns a Gizoogle search
+
+  Example:
+
+      iex> Gizoogle.url("https://example.com")
+      "http://www.gizoogle.net/tranzizzle.php?search=https%3A%2F%2Fexample.com&se=Go+Git+Dis+Shiznit"
+  """
+  @spec url(String.t) :: String.t
+  def url(query) do
+    params = URI.encode_query search: query, se: "Go Git Dis Shiznit"
+    "#{@base_url}/tranzizzle.php?#{params}"
   end
 end
--- test/gizoogle_test.exs
+++ test/gizoogle_test.exs
@@ -8,4 +8,10 @@ defmodule GizoogleTest do
     output = "This be a cold-ass lil straight-up aiiight n' regular english sentence"
     assert Gizoogle.translate(input) == output
   end
+
+  test "url buildz a search strang from a query" do
+    query = "https://example.com"
+    expected = "http://www.gizoogle.net/tranzizzle.php?search=https%3A%2F%2Fexample.com&se=Go+Git+Dis+Shiznit"
+    assert Gizoogle.url(query) == expected
+  end
 end
